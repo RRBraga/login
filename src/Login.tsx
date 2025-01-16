80edcecb05a0bb00c55f5b3e9ae7075e7fe8bf3f
RRBraga <rafaelreis.rrb@gmail.com>
2025-01-16 17:39:11 -0300

    Feat: Criando novo projeto - Tela de login

---
+++ src/Login.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material"
+
+const Login = () => {
+  
+  return (
+    <Typography 
+        variant="h1" align="center">
+            Login
+    </Typography>
+
+  )
+}
+
+export default Login
