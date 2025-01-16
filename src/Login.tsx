20291ffa4dabcc11152a19ff555e277ab41f4ddf
RRBraga <rafaelreis.rrb@gmail.com>
2025-01-16 18:03:33 -0300

    Feat: filds

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,13 +1,36 @@
-import { Typography } from "@mui/material"
+import { Box, Button, TextField, Typography } from "@mui/material"
 
 const Login = () => {
   
   return (
-    <Typography 
-        variant="h1" align="center">
-            Login
-    </Typography>
-
+    <Box
+    bgcolor="#7159c1"
+    display="flex"
+    flexDirection="column"
+    >
+        <Typography 
+            variant="h1" align="center">
+                Sing in
+        </Typography>
+        
+        <Box
+        bgcolor="#7159c1"
+        display="flex"
+        flexDirection="column"
+        >
+            <TextField
+                type="email"
+                placeholder="E-mail"
+                variant="outlined"
+            />
+            <TextField
+                type="password"
+                placeholder="Password"
+                variant="outlined"
+            />
+            <Button variant="contained">Contained</Button>
+        </Box>
+    </Box>
   )
 }
 
